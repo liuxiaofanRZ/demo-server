7e4e7eba3b032c6407996f13fcb0920778a3bfae
zxkj_liuxiaofan <1431083935@qq.com>
2022-12-14 17:47:42 +0800

    feat:增加Result的语法糖

--- src/main/java/com/example/demo/util/Result.java
+++ src/main/java/com/example/demo/util/Result.java
@@ -12,7 +12,7 @@ public class Result<T> implements Serializable {
     private String message = "";
     private T result;
 
-    public static<T> Result<T> ok(T data, String msg) {
+    public static <T> Result<T> ok(T data, String msg) {
         Result<T> r = new Result<T>();
         r.setSuccess(true);
         r.setCode(CommonConstant.SC_OK_200);
@@ -20,7 +20,25 @@ public class Result<T> implements Serializable {
         r.setMessage(msg);
         return r;
     }
-    public static<T> Result<T> error(String msg, T data) {
+
+    // 删除
+    public static <T> Result<T> deleteOk(T data) {
+        return Result.ok(data, "删除成功");
+    }
+    // 添加
+    public static <T> Result<T> addOk(T data) {
+        return Result.ok(data, "添加成功");
+    }
+    // 查询
+    public static <T> Result<T> listOk(T data) {
+        return Result.ok(data, "查询成功");
+    }
+    // 编辑
+    public static <T> Result<T> editOk(T data) {
+        return Result.ok(data, "编辑成功");
+    }
+
+    public static <T> Result<T> error(String msg, T data) {
         Result<T> r = new Result<T>();
         r.setSuccess(false);
         r.setCode(CommonConstant.SC_INTERNAL_SERVER_ERROR_500);
