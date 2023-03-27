daac0586e1562b40e758b8f1e0af88a436701e5f
zxkj_liuxiaofan <1431083935@qq.com>
2023-03-27 10:06:58 +0800

    feat:去掉platform

--- src/main/java/com/example/demo/mapper/PlatformMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.demo.entity.Platform;
-import org.mapstruct.Mapper;
-
-@Mapper
-public interface PlatformMapper extends BaseMapper<Platform> {
-}
--- src/main/java/com/example/demo/service/PlatformService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.demo.entity.Platform;
-
-public interface PlatformService extends IService<Platform> {
-}
