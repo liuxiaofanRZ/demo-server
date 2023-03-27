0fbbc0330376e051906520b68b2c9186fb961396
zxkj_liuxiaofan <1431083935@qq.com>
2023-03-27 09:56:36 +0800

    refactor:mapper的注解改为xml

--- src/main/java/com/example/demo/mapper/MenuMapper.java
+++ src/main/java/com/example/demo/mapper/MenuMapper.java
@@ -4,13 +4,14 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.entity.Menu;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
 @Mapper
 public interface MenuMapper extends BaseMapper<Menu> {
-
-    @Update("update t_menu set is_leaf=#{leaf} where id=#{id}")
+    // 用注解的话，见下行注释部分，不用注解则需要将sql写到MenuMapper.xml中
+    //    @Update("update t_menu set is_leaf=#{leaf} where id=#{id}")
     public int setMenuLeaf(String id, int leaf);
 }
